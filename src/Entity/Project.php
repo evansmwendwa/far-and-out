4309735420e325385d40c0f2d56d321e694c5081
Evans Mwendwa <evansmwendwa@live.com>
2018-01-02 16:03:13 +0300

    add project entity

---
+++ src/Entity/Project.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Entity\BaseEntity;
+use App\Traits\SlugTrait;
+use App\Traits\ImageTrait;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProjectRepository")
+ */
+class Project extends BaseEntity
+{
+    use SlugTrait;
+
+    use ImageTrait;
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+}
