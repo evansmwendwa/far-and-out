58bce01a659e0a944ba44bdd4b3526fa1718a95d
Evans Mwendwa <evansmwendwa@live.com>
2018-01-14 00:24:41 +0300

    add content fields to project entity

--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -4,9 +4,11 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use App\Entity\BaseEntity;
+use App\Utils\Utility;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProjectRepository")
+ * @ORM\HasLifecycleCallbacks()
  */
 class Project extends BaseEntity
 {
@@ -20,4 +22,63 @@ class Project extends BaseEntity
      * @ORM\Column(type="integer")
      */
     protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=50, nullable=true)
+     */
+    protected $category;
+
+    /**
+     * @ORM\Column(type="string", length=50, nullable=true)
+     */
+    protected $companyName;
+
+    /**
+     * Get the value of Category
+     *
+     * @return mixed
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * Set the value of Category
+     *
+     * @param mixed category
+     *
+     * @return self
+     */
+    public function setCategory($category)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Company Name
+     *
+     * @return mixed
+     */
+    public function getCompanyName()
+    {
+        return $this->companyName;
+    }
+
+    /**
+     * Set the value of Company Name
+     *
+     * @param mixed companyName
+     *
+     * @return self
+     */
+    public function setCompanyName($companyName)
+    {
+        $this->companyName = $companyName;
+
+        return $this;
+    }
+
 }
---
+++ src/Migrations/Version20180113211019.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180113211019 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE project ADD category VARCHAR(50) DEFAULT NULL, ADD company_name VARCHAR(50) DEFAULT NULL, ADD description VARCHAR(180) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE project DROP category, DROP company_name, DROP description');
+    }
+}
